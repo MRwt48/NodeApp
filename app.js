5d5477309b8d213b24f69806c10fdddf1713db48
Mukul Rawat <mukul.rawat.3748@gmail.com>
2020-02-12 23:12:10 +0530

    commit 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true })); // bodty parsing for form response
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // bodty parsing for form re
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use("/", (req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "error.html"));
--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,10 @@ const path = require("path");
 
 const routes = express.Router();
 
+const adminData = require("./admin"); //to import products
+
 routes.get("/", (req, res, next) => {
+  console.log(adminData.products);
   // res.send("<h1>Hello</h1>"); //if not next then call res
   // res.sendFile(path.join(__dirname, "views", "shop.html")); //
   res.sendFile(path.join(__dirname, "..", "views", "shop.html")); // the oter method using __dirname
